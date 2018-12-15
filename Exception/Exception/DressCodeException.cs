321a322d3b36eb2b238080191c70bc33f5b77298
CatalinPalcu <palcu_catalin@yahoo.com>
2018-12-15 12:16:44 +0200

    Added custom exception

---
+++ Exception/Exception/DressCodeException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exception
+{
+    class DressCodeException:ApplicationException
+    {
+        private string message = "You are not wering an Elf costume, and you are underage!";
+
+        public DressCodeException()
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
--- Exception/Exception/Program.cs
+++ Exception/Exception/Program.cs
@@ -6,7 +6,59 @@ namespace Exception
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+            try
+            {
+                int a = 0;
+                int b = 100 / a;
+
+                Console.WriteLine(a);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine(ex.Source);
+            }
+            catch (System.Exception error)
+            {
+                Console.WriteLine(error.Message);
+            }*/
+            Person pers1;
+            Start:
+
+            try
+            {
+                Console.WriteLine("Give the age");
+                int age = Int32.Parse(Console.ReadLine());
+                 pers1 = new Person(age);
+            }/*
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message + ex.GetType());
+                goto Start;
+            }*/
+            
+            catch (ArgumentException ex)
+            {
+
+                Console.WriteLine(ex.Message + ex.GetType());
+                goto Start;
+            }
+            catch (DressCodeException ex)
+            {
+                Console.WriteLine(ex.Message + ex.GetType());
+                goto Start;
+            }
+            finally
+            {
+                Console.WriteLine("Finally");
+            }
+            
+
+            Console.WriteLine("The age is: "+pers1.Age);
+            Console.ReadLine();
+
         }
     }
 }
